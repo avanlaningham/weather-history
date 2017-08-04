54383ec7660e6bc5a4936303f4e8a3d22af393af
Alex VanLaningham <av@totxt.net>
2017-08-04 12:34:54 +0200

    removing xml dependency

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var request = require('request');
-var parser = require('xml2json');
 var moment=require('moment');
 var Promise = require('bluebird');
 
@@ -8,18 +7,16 @@ exports.getWeatherHistory = function(lat,lon, timeIn,apiKey){
 	var date = time.format('YYYY-MM-DD');
 	var hourIn = time.hour();
 	var query=lat +',' + lon;
-	var url="https://api.apixu.com/v1/history.xml?key=" + apiKey + " &q="+ query +"&dt=" + date;
+	var url="https://api.apixu.com/v1/history.json?key=" + apiKey + " &q="+ query +"&dt=" + date;
 	return new Promise(function(resolve, reject){
 		request(url, function (error, response, body) {
-			var jsonStr = parser.toJson(body);
-			var json = JSON.parse(jsonStr);
-			if (!isObject(json.root)){
+			if (!isObject(body.forecast)){
 				var weather={};
 				weather.temp_c=15;
 				weather.isValid=false;
 				resolve(weather); //set default temp to 15 if unknown
 			}else{
-				var history = json.root.forecast;
+				var history = body.forecast;
 				var hourArray =history.forecastday.hour;
 				var final;
 				var weather={};
